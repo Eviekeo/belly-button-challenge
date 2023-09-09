a628a0edbacb4c920e651e86677f5564a7f98782
eviekeo <evie.ckeo@gmail.com>
2023-09-09 19:58:08 +1000

    submitting assignment

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # belly-button-challenge
+In this assignment, I have built an interactive dashboard to explore the Belly Button Biodiversity data, which catalogues  microbes that colonise human navels.
+
+The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+## Dashboard
+The dashboard has been deployed on github-pages. See below for the link:
+        https://eviekeo.github.io/belly-button-challenge/
+
+## Code Source - used in app.js
+
+        let sampleArray = sampledata.filter(number => number.id == sampleid)[0];
+The above code was used to filter the sampledata to find the sampleid
+The code was adapted from the below site: https://blog.debugeverything.com/how-to-use-arrow-functions-with-javascript-filter/
+
+                var bubbledata = [{
+            x: OTUids ,
+            y: samplevalues ,
+            text: OTUlabels,
+            mode: 'markers',
+            marker: {
+              color:OTUids,
+              size: samplevalues,
+              colorscale: "Earth"
+            }
+          }];
+          
+          
+          
+          var bubblelayout = {
+            title: 'Bubble Chart',
+            showlegend: false,
+
+          };
+          
+          Plotly.newPlot('bubble', bubbledata, bubblelayout);
+The above code was used to create the bubble graph and was adapted from the below site:
+https://plotly.com/javascript/bubble-charts/
+
+
+                Object.entries(sampleArray).forEach(entry => {
+            const [key, value] = entry;
+            console.log(key, value);
+            tablemenu.append("h5").text(`${key}: ${value}`);
+          });
+The above code was used to searcg through the metadata and obtain key value pairs.
--- static/js/app.js
+++ static/js/app.js
@@ -1,47 +1,106 @@
 // Store the URL
 const URL = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-//let buttondata;
 
-// Initialise the page with a default plot
+// Initialise populating the dropdown menu
 function init() {
-    d3.json(URL).then((data) => {
-        buttondata = data;
-    DropDown(buttondata);
+    // Fetch the json data and console log it
+    d3.json(URL).then(function(data) {
+        let button_data = data.samples
+        let names = data.names
+        console.log(data);
+    
+    
+    // Populate the dropdown menu
+    let dropdownMenu = d3.select("#selDataset");
+        names.forEach((id) => {
+        dropdownMenu.append("option").text(id);
+    });
+
+    // Obtain the first sample ID from the names list
+    let ID_one = names[0];
+
+    // Filter based on the value of the sample
+    charts(ID_one)
+    metatable(ID_one)
 
 });
 };
 
-// This function is called when a dropdown menu item is selected
-function DropDown (data) {
-    // Use D3 to select the dropdown menu
-    let dropdownMenu = d3.select("#selDataset");
-    // Extract the names from the data dictionary and store into a variable
-    let names = data.names;
+init();
+
+// Function to update the data when the sampleid is changed
+function optionChanged(sampleid) {
+    charts(sampleid)
+    metatable(sampleid)
 }
 
+// Function for the metadata table
+function metatable(sampleid) {
+    d3.json(URL).then(function(data) {
+        let sampledata = data.metadata
+        let sampleArray = sampledata.filter(number => number.id == sampleid)[0];
+        let tablemenu = d3.select("#sample-metadata");
+        // Clear the metadata
+        tablemenu.html("")
+        // Loop through the array and append the key value pairs to the tablemenu 
+        Object.entries(sampleArray).forEach(entry => {
+            const [key, value] = entry;
+            console.log(key, value);
+            tablemenu.append("h5").text(`${key}: ${value}`);
+          });
+    });
+}
 
-    
-    
-// Use D3 to select the dropdown menu
-//let dropdownMenu = d3.select("#selDataset");
-    
-    
-    
-    
-    
-    
+// Function to create the bar and bubble chart
+function charts(sampleid) {
+    d3.json(URL).then(function(data) {
+        let sampledata = data.samples
+        // Filter based on sampleid value 
+        let sampleArray = sampledata.filter(number => number.id == sampleid)[0];
+        // Obtain the corresponding otu_ids, sample_values and otu_labels 
+        let OTUids = sampleArray.otu_ids;
+        let samplevalues = sampleArray.sample_values;
+        let OTUlabels = sampleArray.otu_labels;
+        // Set up the bubble chart
+        var bubbledata = [{
+            x: OTUids ,
+            y: samplevalues ,
+            text: OTUlabels,
+            mode: 'markers',
+            marker: {
+              color:OTUids,
+              size: samplevalues,
+              colorscale: "Earth"
+            }
+          }];
+                    
+          var bubblelayout = {
+            title: 'Bubble Chart',
+            showlegend: false,
+          };
+          
+          Plotly.newPlot('bubble', bubbledata, bubblelayout);
+          
+          // Create the bar chart - obtain the top 10 otu_id valuesby slicing the data and reversing the order
+          var bardata = [{
+            x: samplevalues.slice(0,10).reverse(),
+            y: OTUids.slice(0,10).map(x => `OTU ${x}`).reverse(),
+            text: OTUlabels.slice(0,10).reverse(),
+            orientation: 'h',
+            type: 'bar'
+          }];
+          
+                    
+          var barlayout = {
+            title: 'Bar Chart',
+          };
+          
+          Plotly.newPlot('bar', bardata, barlayout);
+    });
+} 
 
 
-// Fetch the JSON data and console log it
-//d3.json(URL).then(function(data) {
-//  console.log(data);
 
 
-//    let samples_values = data.samples[0].sample_values;
-//    console.log(samples_values)
 
-//    let 
-//});
-
-console.log("hello");
